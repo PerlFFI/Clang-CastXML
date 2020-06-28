5e1010a619d1de38c78778b26453d35e165b7935
Graham Ollis <plicease@cpan.org>
2020-06-27 23:18:12 -0600

    add to_href

--- lib/Clang/CastXML/Container.pm
+++ lib/Clang/CastXML/Container.pm
@@ -3,6 +3,7 @@ package Clang::CastXML::Container;
 use Moo;
 use 5.020;
 use experimental qw( signatures );
+use XML::Parser;
 
 # ABSTRACT: Container class for XML output from CastXML
 # VERSION
@@ -39,6 +40,51 @@ Returns the raw XML as a utf-8 string.
 sub to_xml ($self)
 {
   return $self->dest->slurp_utf8;
+}
+
+=head2 to_href
+
+=cut
+
+sub to_href ($self)
+{
+  my $cur = { inner => [] };
+  my @stack;
+
+  my $parser = XML::Parser->new(
+    Handlers => {
+      Start => sub ($, $element, %attrs) {
+
+        # Fixups
+        delete $attrs{location}; # redundant
+        if($element eq 'Function')
+        {
+          my $bad_mangle = '_Z' . length($attrs{name}) . $attrs{name};
+          delete $attrs{mangled} if $attrs{mangled} eq $bad_mangle;
+        }
+
+        my $inner = ( $cur->{inner} //= [] );
+        push @stack, $cur;
+        $cur = {
+          _class => $element,
+          %attrs,
+        };
+        push @$inner, $cur;
+      },
+      End   => sub ($, $element) {
+        my $save = $cur;
+        $cur = pop @stack;
+
+        delete $save->{file} if defined $save->{file} && $save->{file} eq ($cur->{file} // '');
+        delete $save->{line} if defined $save->{line} && $save->{line} eq ($cur->{line} // '');
+      }
+    },
+  );
+
+  my $fh = $self->dest->openr;
+  $parser->parse($fh);
+
+  return $cur->{inner}->[0];
 }
 
 1;
